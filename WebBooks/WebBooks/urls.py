ef68e4fedd1f3afeca4be2019751bcde6ccd499d
Mark77727 <yar-serj@yandex.ru>
2022-07-31 19:01:50 +0500

    Install Commit

---
+++ WebBooks/WebBooks/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from django.contrib import admin
+from django.urls import path
+from catalog import views
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('admin/', admin.site.urls),
+]
