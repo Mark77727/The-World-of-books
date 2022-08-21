374c0daa18b7ef033b699dd9f5a02a3bbe661d03
Sergei Yarushin <work45-ru@mail.ru>
2022-08-21 23:22:25 +0500

    test

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -5,9 +5,15 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path
 from catalog import views
+from django.urls import re_path as url
+
+
+
 
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('admin/', admin.site.urls),
-]
+    url( r'^books/$', views.BookListView.as_view(), name='books'),
+    url( r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    ]
